51ab901797d56b09aa0dc276c190a4b89aaf0644
roleo <roleo@iperbole.bologna.it>
2019-11-15 16:46:12 +0100

    Bug fixing

--- scripts/common.sh
+++ scripts/common.sh
@@ -25,6 +25,7 @@ declare -A CAMERAS
 
 CAMERAS["yi_home_1080p_6FUS_450"]="y203c"
 CAMERAS["yi_home_1080p_9FUS_450"]="y203c"
+CAMERAS["yi_home_1080p_BFUS_450"]="y203c"
 CAMERAS["yi_dome_1080p_6FUS_460"]="h201c"
 CAMERAS["yi_dome_1080p_BFUS_460"]="h201c"
 
