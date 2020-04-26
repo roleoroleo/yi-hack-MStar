a5d4ffc75dabbeaae8fd471650acbc3da13d4b50
roleo <roleo@iperbole.bologna.it>
2020-04-26 16:18:24 +0200

    Add web service to start/stop services

---
+++ src/www/httpd/cgi-bin/service.sh
@@ -0,0 +1,181 @@
+#!/bin/sh
+
+CONF_FILE="etc/system.conf"
+
+YI_HACK_PREFIX="/home/yi-hack"
+
+MODEL_SUFFIX=$(cat /home/yi-hack/model_suffix)
+
+get_config()
+{
+    key=$1
+    grep -w $1 $YI_HACK_PREFIX/$CONF_FILE | cut -d "=" -f2
+}
+
+init_config()
+{
+    if [[ x$(get_config USERNAME) != "x" ]] ; then
+        USERNAME=$(get_config USERNAME)
+        PASSWORD=$(get_config PASSWORD)
+        ONVIF_USERPWD="--user $USERNAME --password $PASSWORD"
+    fi
+
+    case $(get_config RTSP_PORT) in
+        ''|*[!0-9]*) RTSP_PORT=554 ;;
+        *) RTSP_PORT=$(get_config RTSP_PORT) ;;
+    esac
+    case $(get_config RTSP1_PORT) in
+        ''|*[!0-9]*) RTSP1_PORT=8554 ;;
+        *) RTSP1_PORT=$(get_config RTSP1_PORT) ;;
+    esac
+    case $(get_config ONVIF_PORT) in
+        ''|*[!0-9]*) ONVIF_PORT=80 ;;
+        *) ONVIF_PORT=$(get_config ONVIF_PORT) ;;
+    esac
+    case $(get_config HTTPD_PORT) in
+        ''|*[!0-9]*) HTTPD_PORT=8080 ;;
+        *) HTTPD_PORT=$(get_config HTTPD_PORT) ;;
+    esac
+
+    if [[ $RTSP_PORT != "554" ]] ; then
+        D_RTSP_PORT=:$RTSP_PORT
+    fi
+
+    if [[ $RTSP1_PORT != "554" ]] ; then
+        D_RTSP1_PORT=:$RTSP1_PORT
+    fi
+
+    if [[ $HTTPD_PORT != "80" ]] ; then
+        D_HTTPD_PORT=:$HTTPD_PORT
+    fi
+}
+
+start_rtsp()
+{
+    if [[ $1 == "high" ]]; then
+        h264grabber_h -r high | RRTSP_RES=0 RRTSP_PORT=$RTSP_PORT RRTSP_USER=$USERNAME RRTSP_PWD=$PASSWORD rRTSPServer_h >/dev/null &
+    fi
+    if [[ $1 == "low" ]]; then
+        h264grabber_l -r low | RRTSP_RES=1 RRTSP_PORT=$RTSP1_PORT RRTSP_USER=$USERNAME RRTSP_PWD=$PASSWORD rRTSPServer_l >/dev/null &
+    fi
+    if [[ $1 == "both" ]]; then
+        h264grabber_h -r high | RRTSP_RES=0 RRTSP_PORT=$RTSP_PORT RRTSP_USER=$USERNAME RRTSP_PWD=$PASSWORD rRTSPServer_h >/dev/null &
+        h264grabber_l -r low | RRTSP_RES=1 RRTSP_PORT=$RTSP1_PORT RRTSP_USER=$USERNAME RRTSP_PWD=$PASSWORD rRTSPServer_l >/dev/null &
+    fi
+    $YI_HACK_PREFIX/script/wd_rtsp.sh >/dev/null &
+}
+
+stop_rtsp()
+{
+    killall wd_rtsp.sh
+    if [[ $1 == "high" ]]; then
+        killall h264grabber_h
+        killall rRTSPServer_h
+    fi
+    if [[ $1 == "low" ]]; then
+        killall h264grabber_l
+        killall rRTSPServer_l
+    fi
+    if [[ $1 == "both" ]]; then
+        killall h264grabber_h
+        killall rRTSPServer_h
+        killall h264grabber_l
+        killall rRTSPServer_l
+    fi
+}
+
+start_onvif()
+{
+    if [[ $2 == "yes" ]; then
+        WATERMARK="&watermark=yes"
+    fi
+    if [[ $1 == "high" ]]; then
+        ONVIF_PROFILE_0="--name Profile_0 --width 1920 --height 1080 --url rtsp://%s$D_RTSP_PORT/ch0_0.h264 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh?res=high$WATERMARK --type H264"
+    fi
+    if [[ $1 == "low" ]]; then
+        ONVIF_PROFILE_1="--name Profile_1 --width 640 --height 360 --url rtsp://%s$D_RTSP1_PORT/ch0_1.h264 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh?res=low$WATERMARK --type H264"
+    fi
+    if [[ $1 == "both" ]]; then
+        ONVIF_PROFILE_0="--name Profile_0 --width 1920 --height 1080 --url rtsp://%s$D_RTSP_PORT/ch0_0.h264 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh?res=high$WATERMARK --type H264"
+        ONVIF_PROFILE_1="--name Profile_1 --width 640 --height 360 --url rtsp://%s$D_RTSP1_PORT/ch0_1.h264 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh?res=low$WATERMARK --type H264"
+    fi
+
+    if [[ $MODEL_SUFFIX == "h201c" ]] ; then
+        onvif_srvd --pid_file /var/run/onvif_srvd.pid --model "Yi Hack" --manufacturer "Yi" --ifs wlan0 --port $ONVIF_PORT --scope onvif://www.onvif.org/Profile/S $ONVIF_PROFILE_0 $ONVIF_PROFILE_1 $ONVIF_USERPWD --ptz --move_left "/home/yi-hack/bin/ipc_cmd -m left" --move_right "/home/yi-hack/bin/ipc_cmd -m right" --move_up "/home/yi-hack/bin/ipc_cmd -m up" --move_down "/home/yi-hack/bin/ipc_cmd -m down" --move_stop "/home/yi-hack/bin/ipc_cmd -m stop" --move_preset "/home/yi-hack/bin/ipc_cmd -p"
+    else
+        onvif_srvd --pid_file /var/run/onvif_srvd.pid --model "Yi Hack" --manufacturer "Yi" --ifs wlan0 --port $ONVIF_PORT --scope onvif://www.onvif.org/Profile/S $ONVIF_PROFILE_0 $ONVIF_PROFILE_1 $ONVIF_USERPWD
+    fi
+}
+
+stop_onvif()
+{
+    killall onvif_srvd
+}
+
+start_ftpd()
+{
+    if [[ $1 == "busybox" ]] ; then
+        tcpsvd -vE 0.0.0.0 21 ftpd -w >/dev/null &
+    elif [[ $1 == "pure-ftpd" ]] ; then
+        pure-ftpd -B
+    fi
+}
+
+stop_ftpd()
+{
+    if [[ $1 == "busybox" ]] ; then
+        killall tcpsvd
+    elif [[ $1 == "pure-ftpd" ]] ; then
+        killall pure-ftpd
+    fi
+}
+
+NAME="none"
+ACTION="none"
+PARAM1="none"
+PARAM2="none"
+
+for I in 1 2 3 4
+do
+    CONF="$(echo $QUERY_STRING | cut -d'&' -f$I | cut -d'=' -f1)"
+    VAL="$(echo $QUERY_STRING | cut -d'&' -f$I | cut -d'=' -f2)"
+
+    if [ "$CONF" == "name" ] ; then
+        NAME="$VAL"
+    elif [ "$CONF" == "action" ] ; then
+        ACTION="$VAL"
+    elif [ "$CONF" == "param1" ] ; then
+        PARAM1="$VAL"
+    elif [ "$CONF" == "param2" ] ; then
+        PARAM2="$VAL"
+    fi
+done
+
+init_config
+
+if [ "$ACTION" == "start" ] ; then
+    if [ "$NAME" == "rtsp" ]; then
+        start_rtsp $PARAM1
+    elif [ "$NAME" == "onvif" ]; then
+        start_onvif $PARAM1 $PARAM2
+    elif [ "$NAME" == "ftpd" ]; then
+        start_ftpd $PARAM1
+    elif [ "$NAME" == "mqtt" ]; then
+        mqttv4 >/dev/null &
+    fi
+elif [ "$ACTION" == "stop" ] ; then
+    if [ "$NAME" == "rtsp" ]; then
+        stop_rtsp $PARAM1
+    elif [ "$NAME" == "onvif" ]; then
+        stop_onvif $PARAM1
+    elif [ "$NAME" == "ftpd" ]; then
+        stop_ftpd $PARAM1
+    elif [ "$NAME" == "mqtt" ]; then
+        killall mqttv4
+    fi
+fi
+
+printf "Content-type: application/json\r\n\r\n"
+
+printf "{\n"
+printf "}"
