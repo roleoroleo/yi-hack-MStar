9eec541ae7ca2e5c3bbfd881f74562a4ad85a340
roleo <roleo@iperbole.bologna.it>
2020-05-27 17:04:56 +0200

    Add multiplexer to the start sequence

--- src/static/static/home/yi-hack/script/system.sh
+++ src/static/static/home/yi-hack/script/system.sh
@@ -152,6 +152,8 @@ if [[ $(get_config NTPD) == "yes" ]] ; then
     sleep 5 && ntpd -p $(get_config NTP_SERVER) &
 fi
 
+ipc_multiplexer &
+sleep 1
 if [[ $(get_config MQTT) == "yes" ]] ; then
     mqttv4 &
 fi
