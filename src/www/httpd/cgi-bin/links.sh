bcfd6279b0b71fbce5aa6e277f23eb22879b4927
roleo <roleo@iperbole.bologna.it>
2020-07-17 11:15:30 +0200

    Add audio only link to status page

--- src/www/httpd/cgi-bin/links.sh
+++ src/www/httpd/cgi-bin/links.sh
@@ -40,6 +40,9 @@ if [[ $(get_config RTSP) == "yes" ]] ; then
         printf "\"%s\":\"%s\",\n" "low_res_stream"        "rtsp://$LOCAL_IP$D_RTSP_PORT/ch0_1.h264"
         printf "\"%s\":\"%s\",\n" "high_res_stream"       "rtsp://$LOCAL_IP$D_RTSP_PORT/ch0_0.h264"
     fi
+    if [[ $(get_config RTSP_AUDIO) != "no" ]] && [[ $(get_config RTSP_AUDIO) != "none" ]] ; then
+        printf "\"%s\":\"%s\",\n" "audio_stream"        "rtsp://$LOCAL_IP$D_RTSP_PORT/ch0_2.h264"
+    fi
 fi
 
 printf "\"%s\":\"%s\",\n" "low_res_snapshot"      "http://$LOCAL_IP$D_HTTPD_PORT/cgi-bin/snapshot.sh?res=low&watermark=yes"
