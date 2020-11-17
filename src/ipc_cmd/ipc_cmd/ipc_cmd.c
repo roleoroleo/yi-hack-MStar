922affb375d1420db6f07f7dc2577365cf4cca80
roleo <roleo@iperbole.bologna.it>
2020-11-17 17:43:18 +0100

    Add process fork when starting motion event

--- src/ipc_cmd/ipc_cmd/ipc_cmd.c
+++ src/ipc_cmd/ipc_cmd/ipc_cmd.c
@@ -21,6 +21,7 @@
 
 #include "ipc_cmd.h"
 #include "getopt.h"
+#include "signal.h"
 
 mqd_t ipc_mq;
 
@@ -386,6 +387,42 @@ int main(int argc, char ** argv)
 
     if (start == 1) {
         mq_send(ipc_mq, IPC_MOTION_START, sizeof(IPC_MOTION_START) - 1, 0);
+
+        pid_t pid;
+
+        /* Fork off the parent process */
+        pid = fork();
+        /* An error occurred */
+        if (pid < 0)
+            exit(EXIT_FAILURE);
+         /* Success: Let the parent terminate */
+        if (pid > 0)
+            exit(EXIT_SUCCESS);
+        /* On success: The child process becomes session leader */
+        if (setsid() < 0)
+            exit(EXIT_FAILURE);
+        /* Catch, ignore and handle signals */
+        signal(SIGCHLD, SIG_IGN);
+        signal(SIGHUP, SIG_IGN);
+        /* Fork off for the second time*/
+        pid = fork();
+        /* An error occurred */
+        if (pid < 0)
+            exit(EXIT_FAILURE);
+        /* Success: Let the parent terminate */
+        if (pid > 0)
+            exit(EXIT_SUCCESS);
+        /* Set new file permissions */
+        umask(0);
+        /* Change the working directory to the root directory */
+        /* or another appropriated directory */
+        chdir("/");
+        /* Close all open file descriptors */
+        int x;
+        for (x = sysconf(_SC_OPEN_MAX); x>=0; x--) {
+            close (x);
+        }
+
         sleep(time_to_stop);
         mq_send(ipc_mq, IPC_MOTION_STOP, sizeof(IPC_MOTION_STOP) - 1, 0);
     }
