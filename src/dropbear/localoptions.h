ca5887995e5a9bdc5ab3f7fa40e6f5b738623563
roleo <roleo@iperbole.bologna.it>
2022-06-03 22:22:45 +0200

    Update dropbear to 2022.82

--- src/dropbear/localoptions.h
+++ src/dropbear/localoptions.h
@@ -4,5 +4,6 @@
 #define DSS_PRIV_FILENAME "/home/yi-hack/etc/dropbear/dropbear_dss_host_key"
 #define RSA_PRIV_FILENAME "/home/yi-hack/etc/dropbear/dropbear_rsa_host_key"
 #define ECDSA_PRIV_FILENAME "/home/yi-hack/etc/dropbear/dropbear_ecdsa_host_key"
+#define ED25519_PRIV_FILENAME "/home/yi-hack/etc/dropbear/dropbear_ed25519_host_key"
 
 #endif /* DROPBEAR_LOCALOPTIONS_H */
