778119d41b4e814025ba0f43b99760d405b26ffe
roleo <roleo@iperbole.bologna.it>
2022-06-25 17:54:47 +0200

    Fix bug in getlastrecordedvideo.sh

--- src/www/httpd/cgi-bin/getlastrecordedsnapshot.sh
+++ src/www/httpd/cgi-bin/getlastrecordedsnapshot.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
 # Use. Available variables:
-# "oldness" variable. Defines which video to retrieve. 
+# "oldness" variable. Defines which image to retrieve. 
 # - "0" (default) or "1" means latest already available.
-# - greater than "0" specifies the oldness, so "3" means the third oldest video. Only looks into the current hour folder. So if value is greater than the videos in the latest hour folder, the last one will be sent.
-# - negative value (e.g. "-1") means to wait for the next available video. Only works if a video is already being recorded, max. time to wait 80 seconds.
+# - greater than "0" specifies the oldness, so "3" means the third oldest image. Only looks into the current hour folder. So if value is greater than the iamges in the latest hour folder, the last one will be sent.
+# - negative value (e.g. "-1") means to wait for the next available image. Only works if an image is already being recorded, max. time to wait 80 seconds.
 # "type" variable. Defines what to retrieve.
-# - "1" (default). Gets relative name in the format of DIR/VIDEO.mp4
-# - "2". Gets full URL video.
-# - "3". Gets the video itself as video/mp4 inline.
-# - "4". Gets the video itself as video/mp4 attachment.
+# - "1" (default). Gets relative name in the format of DIR/VIDEO.jpg
+# - "2". Gets full URL image.
+# - "3". Gets the video itself as video/jpg.
 # Examples of use:
-# http://IP:PORT/cgi-bin/getlastrecordedvideo.sh?oldness=-1&type=4 -- Wait for a new video and sends it as an attachment.
-# http://IP:PORT/cgi-bin/getlastrecordedvideo.sh?oldness=-1&type=3 -- Wait for a new video and shows it inline in the browser.
-# http://IP:PORT/cgi-bin/getlastrecordedvideo.sh?type=1            -- Send the relative route of the last available video.
-# http://IP:PORT/cgi-bin/getlastrecordedvideo.sh?oldness=2&type=2  -- Send the URL of the second to last available video.
+# http://IP:PORT/cgi-bin/getlastrecordedsnapshot.sh?oldness=-1&type=3 -- Wait for a new image and shows it in the browser.
+# http://IP:PORT/cgi-bin/getlastrecordedsnapshot.sh?type=1            -- Send the relative route of the last available image.
+# http://IP:PORT/cgi-bin/getlastrecordedsnapshot.sh?oldness=2&type=2  -- Send the URL of the second to last available image.
 
 YI_HACK_PREFIX="/home/yi-hack"
 
@@ -66,10 +64,10 @@ if [ "$OLDNESS" -lt "0" ]; then
         done
 
         # Now we get the number of directories and files in the last modified directory.
-        FILECOUNT=`ls -At /tmp/sd/record/$DIRNAME | grep .mp4 -c`
+        FILECOUNT=`ls -At /tmp/sd/record/$DIRNAME | grep .jpg -c`
         DIRCOUNT=`ls -At /tmp/sd/record/ | grep H -c`
         SLEEPCOUNT=0
-        while [ "$FILECOUNT" -eq `ls -At /tmp/sd/record/$DIRNAME | grep .mp4 -c` ]; do
+        while [ "$FILECOUNT" -eq `ls -At /tmp/sd/record/$DIRNAME | grep .jpg -c` ]; do
             if [ "$SLEEPCOUNT" -gt 800 ]; then
                 # After 80 seconds, we break the wait.
                 break;
@@ -92,11 +90,11 @@ for f in `ls -At /tmp/sd/record | grep H`; do
 done
 # In $DIRNAME we now has the last modified directory.
 
-COUNT=`ls -At /tmp/sd/record/$DIRNAME | grep .mp4 -c`
+COUNT=`ls -At /tmp/sd/record/$DIRNAME | grep .jpg -c`
 IDX=1
-for f in `ls -At /tmp/sd/record/$DIRNAME | grep .mp4`; do
+for f in `ls -At /tmp/sd/record/$DIRNAME | grep .jpg`; do
     if [ ${#f} == 12 ] || [ ${#f} == 12 ]; then
-        VIDNAME="$f"
+        IMGNAME="$f"
         if [ "$IDX" -ge "$OLDNESS" ]; then
             break;
         elif [ "$IDX" == "$COUNT" ]; then
@@ -111,18 +109,18 @@ if [ "$TYPE" == "2" ]; then
     source /home/yi-hack/etc/system.conf
 
     printf "Content-type: text/plain\r\n\r\n"
-    echo "http://$LOCAL_IP:$HTTPD_PORT/record/$DIRNAME/$VIDNAME"
+    echo "http://$LOCAL_IP:$HTTPD_PORT/record/$DIRNAME/$IMGNAME"
 elif [ "$TYPE" == "3" ]; then
-    printf "Content-type: video/mp4; charset=utf-8\r\nContent-Disposition: inline; filename=\"$VIDNAME\"\r\n\r\n"
-    cat /tmp/sd/record/$DIRNAME/$VIDNAME
+    printf "Content-type: image/jpeg; charset=utf-8\r\nContent-Disposition: inline; filename=\"$IMGNAME\"\r\n\r\n"
+    cat /tmp/sd/record/$DIRNAME/$IMGNAME
     exit
 elif [ "$TYPE" == "4" ]; then
-    printf "Content-type: video/mp4; charset=utf-8\r\nContent-Disposition: attachment; filename=\"$VIDNAME\"\r\n\r\n"
-    cat /tmp/sd/record/$DIRNAME/$VIDNAME
+    printf "Content-type: image/jpeg; charset=utf-8\r\nContent-Disposition: attachment; filename=\"$IMGNAME\"\r\n\r\n"
+    cat /tmp/sd/record/$DIRNAME/$IMGNAME
     exit
 else
     # Default and type=1
     printf "Content-type: text/plain\r\n\r\n"
-    echo "$DIRNAME/$VIDNAME"
+    echo "$DIRNAME/$IMGNAME"
     exit
 fi
