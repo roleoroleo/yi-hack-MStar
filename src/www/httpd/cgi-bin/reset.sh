2eb42be9d6ef52e75932295e2ab8ac3d6425f24c
roleo <roleo@iperbole.bologna.it>
2022-07-05 10:17:56 +0200

    Fix passwd after reset to default

--- src/www/httpd/cgi-bin/reset.sh
+++ src/www/httpd/cgi-bin/reset.sh
@@ -6,12 +6,7 @@ rm -f hostname
 rm -f TZ
 rm -f passwd
 
-rm camera.conf
-rm mqttv4.conf
-rm proxychains.conf
-rm system.conf
-
-7za x defaults.7z > /dev/null
+7za -y x defaults.7z > /dev/null
 
 printf "Content-type: application/json\r\n\r\n"
 printf "{\n"
