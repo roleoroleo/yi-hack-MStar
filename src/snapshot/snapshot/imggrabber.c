d285f7b3afb3ec7e1357b478d0257fe4154e8041
roleo <roleo@iperbole.bologna.it>
2022-10-07 15:01:02 +0200

    Check if imggrabber is already running

--- src/snapshot/snapshot/imggrabber.c
+++ src/snapshot/snapshot/imggrabber.c
@@ -402,6 +402,49 @@ int add_watermark(unsigned char *buffer, int w_res, int h_res)
     return 0;
 }
 
+pid_t proc_find(const char* process_name, pid_t process_pid)
+{
+    DIR* dir;
+    struct dirent* ent;
+    char* endptr;
+    char buf[512];
+
+    if (!(dir = opendir("/proc"))) {
+        perror("can't open /proc");
+        return -1;
+    }
+
+    while((ent = readdir(dir)) != NULL) {
+        /* if endptr is not a null character, the directory is not
+         * entirely numeric, so ignore it */
+        long lpid = strtol(ent->d_name, &endptr, 10);
+        if (*endptr != '\0') {
+            continue;
+        }
+
+        /* try to open the cmdline file */
+        snprintf(buf, sizeof(buf), "/proc/%ld/cmdline", lpid);
+        FILE* fp = fopen(buf, "r");
+
+        if (fp) {
+            if (fgets(buf, sizeof(buf), fp) != NULL) {
+                /* check the first token in the file, the program name */
+                char* first = strtok(buf, " ");
+                if ((strcmp(first, process_name) == 0) && ((pid_t) lpid != process_pid)) {
+                    fclose(fp);
+                    closedir(dir);
+                    return (pid_t) lpid;
+                }
+            }
+            fclose(fp);
+        }
+
+    }
+
+    closedir(dir);
+    return -1;
+}
+
 void print_usage(char *progname)
 {
     fprintf(stderr, "\nUsage: %s [-m MODEL] [-r RES] [-w] [-d]\n\n", progname);
@@ -528,9 +571,24 @@ int main(int argc, char **argv)
         }
     }
 
+    if (debug) fprintf(stderr, "Starting program\n");
+
     // Check if snapshot is disabled
     if (access("/tmp/snapshot.disabled", F_OK ) == 0 ) {
         fprintf(stderr, "Snapshot is disabled\n");
+        return 0;
+    }
+
+    // Check if snapshot is low res
+    if (access("/tmp/snapshot.low", F_OK ) == 0 ) {
+        fprintf(stderr, "Snapshot is low res\n");
+        resolution = RESOLUTION_LOW;
+    }
+
+    // Check if the process is already running
+    pid_t my_pid = getpid();
+    if (proc_find(basename(argv[0]), my_pid) != -1) {
+        fprintf(stderr, "Process is already running\n");
         return 0;
     }
 
