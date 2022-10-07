ea25a6d0f5e38e061b1710a102621a5bd913023c
roleo <roleo@iperbole.bologna.it>
2022-10-07 11:29:37 +0200

    Add sftp-server

--- src/dropbear/localoptions.h
+++ src/dropbear/localoptions.h
@@ -6,4 +6,11 @@
 #define ECDSA_PRIV_FILENAME "/home/yi-hack/etc/dropbear/dropbear_ecdsa_host_key"
 #define ED25519_PRIV_FILENAME "/home/yi-hack/etc/dropbear/dropbear_ed25519_host_key"
 
+#define DROPBEAR_PATH_SSH_PROGRAM "/bin/dbclient"
+
+#define DEFAULT_PATH "/usr/bin:/usr/sbin:/bin:/sbin:/home/base/tools:/home/app/localbin:/home/base:/home/yi-hack/bin:/home/yi-hack/sbin:/tmp/sd/yi-hack/bin:/tmp/sd/yi-hack/sbin"
+#define DEFAULT_ROOT_PATH "/usr/bin:/usr/sbin:/bin:/sbin:/home/base/tools:/home/app/localbin:/home/base:/home/yi-hack/bin:/home/yi-hack/sbin:/tmp/sd/yi-hack/bin:/tmp/sd/yi-hack/sbin"
+
+#define SFTPSERVER_PATH "/home/yi-hack/usr/libexec/sftp-server"
+
 #endif /* DROPBEAR_LOCALOPTIONS_H */
