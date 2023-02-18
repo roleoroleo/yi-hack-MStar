13a7a6da8dd9bae7907027c11d313129b13e3138
roleo <roleo@iperbole.bologna.it>
2023-02-18 14:36:07 +0100

    Add cloudAPI_fake and change cloudAPI management

--- src/static/static/home/yi-hack/script/system_init.sh
+++ src/static/static/home/yi-hack/script/system_init.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-YI_HACK_PREFIX="/home"
+YI_HACK_PREFIX="/home/yi-hack"
 YI_PREFIX="/home/app"
 
-ARCHIVE_FILE="$YI_HACK_PREFIX/yi-hack/yi-hack.7z"
-DESTDIR="$YI_HACK_PREFIX/yi-hack"
+ARCHIVE_FILE="$YI_HACK_PREFIX/yi-hack.7z"
 
 files=`find $YI_PREFIX -maxdepth 1 -name "*.7z"`
 if [ ${#files[@]} -gt 0 ]; then
@@ -13,16 +12,16 @@ if [ ${#files[@]} -gt 0 ]; then
 fi
 
 if [ -f $ARCHIVE_FILE ]; then
-	/home/base/tools/7za x $ARCHIVE_FILE -y -o$DESTDIR
+	/home/base/tools/7za x $ARCHIVE_FILE -y -o$YI_HACK_PREFIX
 	rm $ARCHIVE_FILE
 fi
 
-if [ ! -f $YI_PREFIX/cloudAPI_real ]; then
-	mv $YI_PREFIX/cloudAPI $YI_PREFIX/cloudAPI_real
-	cp $YI_HACK_PREFIX/yi-hack/script/cloudAPI $YI_PREFIX/
+if [ ! -f $YI_HACK_PREFIX/bin/cloudAPI_real ]; then
+	cp $YI_PREFIX/cloudAPI $YI_HACK_PREFIX/bin/cloudAPI_real
 fi
+mount --bind $YI_HACK_PREFIX/bin/cloudAPI $YI_PREFIX/cloudAPI
 
-mkdir -p $YI_HACK_PREFIX/yi-hack/etc/dropbear
+mkdir -p $YI_HACK_PREFIX/etc/dropbear
 
 # Comment out all the cloud stuff from base/init.sh
 sed -i 's/\t\.\/dispatch \&/\tLD_PRELOAD=\/home\/yi-hack\/lib\/ipc_multiplex.so \.\/dispatch \&/g' /home/app/init.sh
