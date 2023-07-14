4d9848d542f0a959ccaa30d8251b38a768806c48
roleo <roleo@iperbole.bologna.it>
2023-07-14 12:54:35 +0200

    Add missing parameters to mqtt-config

--- src/mqtt-config/mqtt-config/validate.h
+++ src/mqtt-config/mqtt-config/validate.h
@@ -6,7 +6,7 @@
 
 #define PARAM_SIZE    128
 #define PARAM_OPTIONS 9
-#define PARAM_NUM     58
+#define PARAM_NUM     63
 
 int validate_param(char *file, char *key, char *value);
 int extract_param(char *param, char *file, char *key, int index);
