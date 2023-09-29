fb886c7cbad80e4a1580849a623c415847b6ba89
roleo <roleo@iperbole.bologna.it>
2023-09-29 14:13:32 +0200

    Update copyright text

--- src/h264grabber/h264grabber/h264grabber2.c
+++ src/h264grabber/h264grabber/h264grabber2.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 roleo.
+ * Copyright (c) 2023 roleo.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
